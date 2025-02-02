4ba87dc1fa0213ed77c758fff333add4af3a3271
vvk130 <vanessa.kasula@outlook.com>
2025-02-02 19:57:24 +0200

    swagger

--- MyMinimalApi/Program.cs
+++ MyMinimalApi/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
