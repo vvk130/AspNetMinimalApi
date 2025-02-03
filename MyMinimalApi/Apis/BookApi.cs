bcd3bcfe89211774ca4b75a45b770212ffbacb65
vvk130 <vanessa.kasula@outlook.com>
2025-02-03 02:11:37 +0200

    DeleteBook endpoint

--- MyMinimalApi/Apis/BookApi.cs
+++ MyMinimalApi/Apis/BookApi.cs
@@ -1,18 +1,17 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 public static class BookApi
 {
     public static void MapBookApi(this WebApplication app)
     {
         app.MapPost("/books/", CreateBookForAuthor);
+        app.MapDelete("/books/", DeleteBookByName);
     }
 
     public static async Task<Results<Created, NotFound<ProblemDetails>>> CreateBookForAuthor(
         MyDbContext context,
-        [FromBody] BookRequest bookRequest
-    )
+        [FromBody] BookRequest bookRequest)
     {
         var author = await context.Author.FirstOrDefaultAsync(a =>
             a.FirstName == bookRequest.AuthorDto.FirstName
@@ -20,7 +19,8 @@ public static class BookApi
         );
 
         if (author is null)
-            return TypedResults.NotFound<ProblemDetails>(new (){
+            return TypedResults.NotFound<ProblemDetails>(new()
+            {
                 Detail = "Book can only be created for an existing author"
             });
 
@@ -36,5 +36,19 @@ public static class BookApi
         await context.SaveChangesAsync();
 
         return TypedResults.Created();
+    }
+
+    public static async Task<Results<NoContent, NotFound>> DeleteBookByName(
+        MyDbContext context,
+        [FromBody] string title
+    )
+    {
+        var book = await context.Books.FirstOrDefaultAsync(b => b.Title == title);
+        if (book is null)
+            return TypedResults.NotFound();
+
+        context.Books.Remove(book);
+        await context.SaveChangesAsync();
+        return TypedResults.NoContent();
     }
 }
