5b117ac6a8d9b4ddc4def90b9bcf5f253afa1ce2
vvk130 <vanessa.kasula@outlook.com>
2025-02-11 20:07:10 +0200

    Add max pageSize 100

--- MyMinimalApi/Models/PaginationRequest.cs
+++ MyMinimalApi/Models/PaginationRequest.cs
@@ -1,4 +1,4 @@
-public record PaginationRequest(
-    int PageSize = 10,
-    int Index = 0
-);
+public record PaginationRequest(int PageSize = 10, int Index = 0)
+{
+    public int PageSize { get; init; } = PageSize > 100 ? 100 : PageSize;
+}
